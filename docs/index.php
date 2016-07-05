29fb5412c3e90833f9223ca7cfe0c50d4bed10df
Mike Wicks <mike.wicks@blueyonder.co.uk>
2016-07-05 13:56:34 +0100

    Tuesday 5th July 2016
    Initial Commit

---
+++ docs/index.php
@@ -0,0 +1,27 @@
+<?PHP
+	include $_SERVER['DOCUMENT_ROOT'].'/../secure/includes/database_connect.inc';
+
+	$path = explode('/',$_SERVER['REQUEST_URI']);
+	
+	if ($path[1] == "chickens") {
+    
+        $line = $path[2];
+        
+        $query = "SELECT name FROM chickenLines WHERE REPLACE(name, ' ', '-') LIKE ?";
+        
+        $parameters = array("s", $line);
+        
+        $result = bePrepared($parameters,$query,$database);
+
+        if (mysqli_num_rows($result) == '1') {
+            $row = mysqli_fetch_object($result);
+            header("HTTP/1.0 200 OK");
+            include $_SERVER['DOCUMENT_ROOT']."/chickens/line-page.html";
+            die(); 
+        }
+    }
+	else {
+
+		echo "<h1>Not Found - index.php</h1>"; 
+	}
+?>
